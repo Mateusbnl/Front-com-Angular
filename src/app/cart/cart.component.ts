7bb8e7b1b0eb6057b556ba0d12e14fb72dfaa1c8
Mateusbnl <mateusbnl@hotmail.com>
2022-07-06 22:16:01 -0300

    Usando Form com Angular

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
   items = this.cartService.getItems();
 
-  ngOnInit(): void {
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Seu pedido foi enviado', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
+  ngOnInit(): void {}
 }
